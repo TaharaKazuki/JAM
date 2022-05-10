4c8976f7a02cbe774f9275b5c7b52aba5d061cab
Tahara Kazuki <>
2022-05-10 23:57:54 +0900

    first setting

--- server.ts
+++ server.ts
@@ -1,1 +1,11 @@
-console.info('server')
+import type { Express } from 'express'
+import express from 'express'
+
+const app: Express = express()
+const PORT = 3000
+
+app.get('/', (req, res) => {
+  res.send('hello express')
+})
+
+app.listen(PORT, () => console.info('start server'))
