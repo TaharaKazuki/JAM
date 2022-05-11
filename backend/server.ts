7c1dc8c3daad0cc6dfb1bdfb2b95b3ba91ac6cbb
Tahara Kazuki <>
2022-05-11 18:42:10 +0900

    add router

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,14 @@
 import type { Express } from 'express'
 import express from 'express'
 import useRouter from './routes/users'
+import authRouter from './routes/auth'
+import postsRouter from './routes/posts'
 
 const app: Express = express()
 const PORT = 3000
 
 app.use('/api/users', useRouter)
+app.use('api/auth', authRouter)
+app.use('api/posts', postsRouter)
 
 app.listen(PORT, () => console.info('start server'))
