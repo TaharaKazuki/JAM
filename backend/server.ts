c4f8196a32a82517bc7a5fb437aab8f703b9633f
Tahara Kazuki <>
2022-05-11 18:35:34 +0900

    add user route

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,10 @@
 import type { Express } from 'express'
 import express from 'express'
+import useRouter from './routes/users'
 
 const app: Express = express()
 const PORT = 3000
 
-app.get('/', (req, res) => {
-  res.send('hello express')
-})
+app.use('/api/users', useRouter)
 
 app.listen(PORT, () => console.info('start server'))
