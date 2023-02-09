d77f009ce06bf3a871a7f86593f5d08d97bb4872
Tahara Kazuki <ssp0727.lnc@gmail.com>
2023-02-10 00:26:26 +0900

    update

--- backend/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { register, loginUser } from '../controllers/authController'
-
-const router = Router()
-
-router.route('/auth/register').post(register)
-router.route('/auth/login').post(loginUser)
-
-export default router
--- backend/routes/users.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { getUser, updateUser, deleteUser } from '../controllers/usersController'
-
-const router = Router()
-
-router.route('/users/:id').get(getUser).put(updateUser).delete(deleteUser)
-
-export default router
