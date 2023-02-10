78f9e3a0e336162a397a10b80a1384269c59f666
Tahara Kazuki <ssp0727.lnc@gmail.com>
2023-02-10 21:55:43 +0900

    update

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth'
-import FacebookProvider from 'next-auth/providers/facebook'
 import GoogleProvider from 'next-auth/providers/google'
+import FacebookProvider from 'next-auth/providers/facebook'
+import GitHubProvider from 'next-auth/providers/github'
 
 export default NextAuth({
   providers: [
@@ -11,6 +12,10 @@ export default NextAuth({
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID as string,
       clientSecret: process.env.FACEBOOK_SECRET as string,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
 })
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
+import { useSession, signIn, signOut, getSession } from 'next-auth/react'
+import { NextPageContext } from 'next'
+
 export default function Home() {
+  const { data: session } = useSession()
+  console.info(session)
   return (
     <>
       <h1 className="text-red-700 text-xl">Welcome</h1>
+      <button onClick={() => signIn()}>Sign In</button>
+      <button onClick={() => signOut()}>Sign out</button>
     </>
   )
+}
+
+export async function getServerSideProps(ctx: NextPageContext) {
+  const session = await getSession(ctx)
+
+  return {
+    props: {
+      session,
+    },
+  }
 }
