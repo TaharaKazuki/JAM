49c471816e7babd25a9ef75a22b78469f1110bf0
Tahara Kazuki <ssp0727.lnc@gmail.com>
2023-02-12 13:32:15 +0900

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,29 @@
 import { useSession, signIn, signOut, getSession } from 'next-auth/react'
 import { NextPageContext } from 'next'
+import Image from 'next/image'
 
 export default function Home() {
   const { data: session } = useSession()
   console.info(session)
   return (
     <>
-      <h1 className="text-red-700 text-xl">Welcome</h1>
-      <button onClick={() => signIn()}>Sign In</button>
-      <button onClick={() => signOut()}>Sign out</button>
+      <h1 className="text-red-700 text-4xl bg-yellow-300">{session?.user?.name}</h1>
+      <Image
+        src={session?.user?.image as string}
+        alt=""
+        width="128"
+        height="128"
+        className="rounded-full w-[128px] h-32"
+      />
+      {session ? (
+        <button onClick={() => signOut()} className="bg-blue-400">
+          SignOut
+        </button>
+      ) : (
+        <button onClick={() => signIn()} className="bg-blue-400">
+          SignIn
+        </button>
+      )}
     </>
   )
 }
